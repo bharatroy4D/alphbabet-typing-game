94bb3a04ef6604e750152bd72f03fd08a5a0bd5f
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-01-31 23:21:15 +0600

    play ground hidden class remove

--- alpha-cash.js
+++ alpha-cash.js
@@ -1,5 +1,7 @@
 // play function
-function play(){
+function play() {
     const homeScreen = document.getElementById('home-screen');
-        homeScreen.classList.add('hidden')
+    homeScreen.classList.add('hidden');
+    const playGround = document.getElementById('play-ground');
+    playGround.classList.remove('hidden');
 }
