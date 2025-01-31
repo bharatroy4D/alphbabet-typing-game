59f906756eef7419a64545939b122f3629fe694c
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-01-31 23:34:50 +0600

    functtion by home and play ground class added and remove

--- alpha-cash.js
+++ alpha-cash.js
@@ -1,7 +1,11 @@
 // play function
 function play() {
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    const playGround = document.getElementById('play-ground');
-    playGround.classList.remove('hidden');
+    // home screen hidden
+    // const homeScreen = document.getElementById('home-screen');
+    // homeScreen.classList.add('hidden');
+    // play ground show
+    // const playGround = document.getElementById('play-ground');
+    // playGround.classList.remove('hidden');
+    hideElementById('home-screen');
+    showElementById('play-ground')
 }
