12bbde89edb8880d70da637307423cbf50c2f4cf
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-01 10:56:02 +0600

    random number and alphabet complite

--- alpha-cash.js
+++ alpha-cash.js
@@ -1,11 +1,10 @@
+function continueGame(){
+const alphabets = getRandomAlphabet();
+const displayLatter = document.getElementById('')
+}
 // play function
 function play() {
-    // home screen hidden
-    // const homeScreen = document.getElementById('home-screen');
-    // homeScreen.classList.add('hidden');
-    // play ground show
-    // const playGround = document.getElementById('play-ground');
-    // playGround.classList.remove('hidden');
     hideElementById('home-screen');
     showElementById('play-ground')
+    continueGame();
 }
--- utility.js
+++ utility.js
@@ -1,9 +1,23 @@
+// hidden element id
 function hideElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
-
+// show element id
 function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+
+function getRandomAlphabet(){
+    // random alphabet
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('')
+
+    // random number
+    const randomNumbers = Math.random() * 25;
+    const index = Math.round(randomNumbers);
+    const alphabet = alphabets[index];
+    return alphabet;
+
 }
